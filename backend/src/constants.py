f00288cac825a202dbcbb0e07fedf4e8af9fb1b1
Caio <caiofeliperro@gmail.com>
2025-01-09 22:13:26 -0300

    feat: add crud and constants files

---
+++ backend/src/constants.py
@@ -0,0 +1,11 @@
+PERMISSIONS = [
+    {"name": "manage:questions", "description": "Manage the bank of questions"},
+    {"name": "manage:users", "description": "Manage users"},
+    {"name": "manage:quizzes", "description": "Manage quizzes"},
+    {"name": "manage:classrooms:all", "description": "Manage all classrooms"},
+    {"name": "manage:classrooms", "description": "Manage classrooms (user is teacher)"},
+    {"name": "view:questions", "description": "View questions (bank of questions)"},
+    {"name": "view:users", "description": "View users (all users)"},
+    {"name": "view:quizzes", "description": "View quizzes (all quizzes)"},
+    {"name": "view:classrooms", "description": "View classrooms (all classrooms)"},
+]
