e30d99e47e4ab5dbe23adec660a681a907bd1b24
MatheussBrant <matheus.s.brant@gmail.com>
2025-02-10 12:53:19 -0300

    Fix env

--- frontend/services/config.ts
+++ frontend/services/config.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.API_URL
+export const API_URL = process.env.NEXT_PUBLIC_API_URL
 
 export const getAuthHeaders = () => {
   const token = localStorage.getItem("token");
